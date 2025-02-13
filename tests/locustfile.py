a1e17e93f38415e3a4decdd1d13d45684eb312e2
Mahdi <mahdiiporheydarii@gmail.com>
2025-02-14 02:29:38 +0330

    Modify tests

--- tests/locustfile.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task, between
-
-class RecommendationUser(HttpUser):
-    wait_time = between(1, 3)  # Simulates real-world user behavior
-
-    @task
-    def get_recommendations(self):
-        self.client.get("/recommendations/1")  # Replace 1 with a test user ID
